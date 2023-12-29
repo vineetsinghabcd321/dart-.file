f30c7736396193e2b1c32683408cd54a79a21090
vineetsinghabcd321 <vineetsingh953250@gmail.com>
2023-12-29 23:46:20 +0530

    first commit

---
+++ dart_project/basic&operators dart files/normalques.dart
@@ -0,0 +1,7 @@
+void main() {
+
+int length = 10, width = 12;
+
+ int area = length * width;
+print(area);
+}
--- dart_project/basic&operators dart files/operators1.dart
+++ dart_project/basic&operators dart files/operators1.dart
@@ -1,11 +1,7 @@
 //Write a Dart program to calculate the area of a rectangle using variables for length and width. Print the result.
 
 void main() {
-
-    int length = 12;
-     int width = 8;
-    
-     int area = length * width;
-
-    print(area);
+  int length = 6, width = 9;
+    int area = length * width;
+     print(area );
 }
